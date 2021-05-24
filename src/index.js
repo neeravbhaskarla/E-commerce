86ec34bfcc502d0be33b32fa0d8752c4a2e2ac2b
neerav <ananthaneerav@gmail.com>
2021-05-25 01:28:59 +0530

    added production build Tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './assets/style/main.css'
 import App from './App';
 import {StoreContextProvider} from './store/use-context'
 ReactDOM.render(
